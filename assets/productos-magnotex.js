56bb6c44951735840eca30423dd6baf91829eb45
Roberto <andval19971997@gmail.com>
2026-08-08 22:17:12 +0200

    Organiza y restaura los recursos visuales

---
+++ assets/productos-magnotex.js
@@ -0,0 +1,19 @@
+window.MAGNOTEX_PRODUCTS={
+  allProducts:[
+    {n:"Almohadas",img:"assets/producto-almohadas.png"},
+    {n:"Colchas",img:"assets/producto-colchas.jpg"},
+    {n:"Duvet",img:"assets/producto-duvet.jpg"},
+    {n:"Fundas de duvet",img:"assets/producto-fundas-duvet.jpg"},
+    {n:"Edredones",img:"assets/producto-edredones.jpg"},
+    {n:"Protector de colchón",img:"assets/producto-protector-colchon.jpg"},
+    {n:"Protector de almohada",img:"assets/producto-protector-almohada.jpg"},
+    {n:"Toallas",img:"assets/producto-toallas.png"},
+    {n:"Sábanas",img:"assets/producto-sabanas.png"},
+    {n:"Salidas de baño",img:"assets/producto-salidas-bano.png"},
+    {n:"Faldones",img:"assets/producto-faldones.jpg"},
+    {n:"Cobija térmica",img:"assets/producto-cobija-termica.png"},
+    {n:"Mantelería",img:"assets/producto-manteleria.jpg"},
+    {n:"Alfombras de baño",img:"assets/producto-alfombra-bano.png"}
+  ],
+  featuredProductNames:["Salidas de baño","Sábanas","Toallas","Almohadas"]
+};
